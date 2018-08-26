6cfabec02c9d17780c59057a4dcccbd7cef59fcf
danyn <danyn.com@gmail.com>
2018-08-26 01:07:51 -0700

    working on responsive menu

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -4,17 +4,21 @@ class MobileMenu {
 	constructor() {
 		this.ham = $('.site-header__ham')
 		this.nav = $('.site-header__nav')
+		this.header = $('.site-header')
+		this.contact = $('.site-header__contact')
 		this.events()
 	}
 
 	events(){
 		this.ham.on('click', this.toggleHam.bind(this))
-
 	}
 
 	toggleHam() {
 		console.log(this.nav)
-		this.nav.slideToggle();
+
+		this.nav.toggleClass('site-header__nav--mobile')
+		this.header.toggleClass('site-header--mobile')
+		this.contact.toggleClass('site-header__contact--mobile')
 	}
 }
 
