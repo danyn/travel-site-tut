0e45eab1f1c2ac0246959c77cc204cb8528cc93b
danyn <danyn.com@gmail.com>
2018-08-29 15:26:56 -0700

    add files for modal

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,6 +1,7 @@
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
+import Modal from './modules/Modal'
 
 const menu = new MobileMenu()
 
@@ -8,4 +9,6 @@ new RevealOnScroll('.feature', '80%');
 new RevealOnScroll('.testimonial', '80%');
 
 new StickyHeader();
+
+new Modal();
 
---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,8 @@
+
+class Modal {
+	constructor() {
+		console.log('Modal');
+	}
+}
+
+export default Modal;
